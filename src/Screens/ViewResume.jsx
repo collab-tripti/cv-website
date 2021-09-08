0ecf036457681594747cd72d71540c0010a839be
tripti gupta <gupta.tripti@indiamart.com>
2021-09-08 12:25:08 +0530

    test

--- src/Screens/ViewResume.jsx
+++ src/Screens/ViewResume.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import pic from "../assets/pic.png";
 
 class ViewResume extends Component{
     constructor(props) {    
@@ -57,61 +58,115 @@ class ViewResume extends Component{
         var user = this.props.user?this.props.user:this.props.location.state.user;
 
        return (
-                <div className="formDiv col-lg-6 fs-5">    
-                <h2 className="d-flex justify-content-around mb-5" >Your Resume</h2>
+                <div >  
+                    <h2 className="d-flex justify-content-around mb-5" >Your Resume</h2>
+                    <div id="cvmakerdiv" class="modal" className="formDiv col-lg-6 fs-5">  
+                     <div id="inner"></div>  
+                <div class="lftsde f1">
+                    <div class="userpic">
+                        <img src={pic} alt="test" style={{"width":"121px","height":"121px","margin": "10px"}} />
+                    </div>
+                
                 <div> 
-                    <strong className="fs-4 mb-0">Personal Information</strong> 
-                        <div className="d-flex col-lg-12">
-                            <label htmlFor="studName">Name:&nbsp;&nbsp;</label> 
-                                {user.studName}   
-                        </div>    
-                        <div className="d-flex col-lg-12">
-                            <div className="d-flex col-lg-6"> 
-                            <label htmlFor="phoneNumber">Contact:&nbsp;&nbsp;</label>  
-                                {user.phoneNumber}        
-                            </div>    
-                            
+                    <strong class="hd_inf wf w100 clrw mt20">Personal Information</strong> 
+                        <div class="wf cg clrw">
+                        <table class="isqdet f1" >
+                        <tbody>
+                            <tr><td class="fldname colr_isq"><label htmlFor="studName">Name:&nbsp;&nbsp;</label> 
+                                    {user.studName}   </td></tr>
+                            <tr><td class="fldname colr_isq"><label htmlFor="text">Date of Birth:&nbsp;&nbsp;</label>
+                                {user.dob}   </td></tr>
+                        </tbody>
+                        </table> 
                         </div>
-                        <div className="d-flex col-lg-12">
-                                <p className="mb-0" htmlFor="emailId">Email Id:&nbsp;&nbsp;</p> 
-                                {user.email}    
+                        </div>   
+
+                    <strong class="hd_inf wf w100 clrw mt20">Contact Information</strong> 
+                        <div class="wf cg clrw">
+                        <table class="isqdet f1" >
+                        <tbody>
+                        <tr><td class="fldname colr_isq"><label htmlFor="phoneNumber">Contact:&nbsp;&nbsp;</label> 
+                                {user.phoneNumber}   </td></tr>
+                            <tr><td class="fldname colr_isq"><p className="mb-0" htmlFor="emailId">Email Id:&nbsp;&nbsp;</p> 
+                                {user.email}   </td></tr>
+                            <tr><td class="fldname colr_isq"><label htmlFor="city">Address:&nbsp;&nbsp;</label>
+                            {user.city}  </td></tr>
+                        </tbody>
+                        </table> 
                         </div>
+
+                        <strong class="hd_inf wf w100 clrw mt20">Social Profile</strong> 
+                        <div class="wf cg clrw">
+                        <table class="isqdet f1" >
+                        <tbody>
+                        <tr><td class="fldname colr_isq">{
+                            user.socialMediaProfile.map((media, id)=> {
+                                return this.socialMediaField(media,id);
+                            })
+                        }</td></tr>
+                        </tbody>
+                        </table> 
+                        </div>
+</div>
+
+
+                        <div class="rgtsde f2">
+	 
+	 <table class="yui-u first">
+     <tbody>
+     <tr><td class="fldname colr_isq"><label htmlFor="studName"></label> 
+                                    {user.studName}   </td></tr>
+                                    <h2 class="cg">System Engineer</h2>      
+				</tbody>		
+	 </table>
+                
+
                         
-                        <div className="d-flex col-lg-12">
-                            <label htmlFor="text">Date of Birth:&nbsp;&nbsp;</label>    
-                                {user.dob}        
-                        </div>
-                    
-                        
-                        <div className="mb-3">
-                            <label htmlFor="city">Address:&nbsp;&nbsp;</label>
-                            {user.city}        
-                        </div>  
-                        <div className="d-flex col-lg-12 justify-content-between"> 
-                            <div className="d-flex flex-column col-lg-4"><strong className="d-block fs-4 mb-0">Education</strong> 
-                            {
+                        <span class="hd_inf wf w100">Work Experience</span>
+          <span class="wf cg job">
+        <table class="isqdet f1" >
+             <tbody>
+             <tr><td class="fldname colr_isq">
+                                {user.experience.map((edu, id) => {
+                                    return this.experienceField(edu,id);
+                                })
+                            }</td><td class="colon">:</td><td><span class="fldvalue"></span></td></tr>
+             </tbody>
+         </table>
+        </span>   
+
+
+                        <span class="hd_inf wf w100">Education</span>
+		  
+          <span class="wf cg job">
+        <table class="isqdet f1" >
+             <tbody>
+             <tr><td class="fldname colr_isq">{
                                 user.education.map((edu, id) => {
                                     return this.educationField(edu,id)
                                 }
                                 )
-                            }</div>
+                            } </td><td class="colon">:</td><td><span class="fldvalue"></span></td></tr>
+             </tbody>
+         </table>
+        </span>        
 
-                            <div className="d-flex flex-column col-lg-6"><strong className="d-block fs-4 mb-0">Work Experience</strong> 
-                            {
-                                user.experience.map((edu, id) => {
-                                    return this.experienceField(edu,id);
-                                })
-                            }</div>
-                        </div>
-                        <strong className="fs-4 mb-0">Social Profile</strong> 
-                        {
-                            user.socialMediaProfile.map((media, id)=> {
-                                return this.socialMediaField(media,id);
-                            })
-                        }
-                </div>  
-                
+                        <span class="hd_inf wf w100">Area Of Expertise</span>
+		  
+          <span class="wf cg job">
+        <table class="isqdet f1" >
+             <tbody>
+             <tr><td class="fldname colr_isq"><label>Institute</label></td><td class="colon">:</td><td><span class="fldvalue">AKG Engg. College</span></td></tr>
+             <tr><td class="fldname colr_isq"><label>Degree</label></td><td class="colon">:</td><td><span class="fldvalue">B.Tech(CS)</span></td></tr>
+             <tr><td class="fldname colr_isq"><label>Languages</label></td><td class="colon">:</td><td><span class="fldvalue">JAVA, C, C++</span></td></tr>
+             <tr><td class="fldname colr_isq"><label>Languages</label></td><td class="colon">:</td><td><span class="fldvalue">JAVA, C, C++</span></td></tr>
+             </tbody>
+         </table>
+        </span>
+            </div>  
+
             </div >
+            </div>
        )
     }
 }
